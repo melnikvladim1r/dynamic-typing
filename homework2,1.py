ca7036a01ac3df960cbe505cbf57f494af968019
melnikvladim1r <162798416+melnikvladim1r@users.noreply.github.com>
2024-03-12 16:38:41 +0300

    Add files via upload

---
+++ homework2,1.py
@@ -0,0 +1,9 @@
+name = "Vladimir"
+print("Name:" ,name)
+age = 44
+print("Age:", age)
+new_age = (age + 1)
+new_age = "45"
+print("New age:",new_age,type(new_age))
+is_student = True
+print("is_student:", is_student,)
